f432a654c8524a9d993e9f17eaec12ea509cb2a4
Petro Sych <petro.sych@careerteacherhub.com>
2024-07-06 09:35:42 +0300

    Add Dashboard page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,14 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 export const routes: Routes = [
-    //login route
     {
-        path: 'login',
+        path: "login",
         component: LoginComponent
-    }
-
+    },
+    {
+        path: "dashboard",
+        component: DashboardComponent
+    },
 ];
