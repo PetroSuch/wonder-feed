e7462882a773353d5313c82f300888742aceb0a0
Petro Sych <petro.sych@careerteacherhub.com>
2024-07-07 13:42:14 +0300

    Navbar generation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,19 @@
-import { Routes } from '@angular/router';
-import { LoginComponent } from './login/login.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
+import { Routes } from "@angular/router";
+import { LoginComponent } from "./login/login.component";
+import { DashboardComponent } from "./dashboard/dashboard.component";
+import { ArticleDashboardComponent } from "./article-dashboard/article-dashboard.component";
 
 export const routes: Routes = [
-    {
-        path: "login",
-        component: LoginComponent
-    },
-    {
-        path: "dashboard",
-        component: DashboardComponent
-    },
+  {
+    path: "login",
+    component: LoginComponent,
+  },
+  {
+    path: "dashboard",
+    component: DashboardComponent,
+  },
+  {
+    path: "articles",
+    component: ArticleDashboardComponent,
+  },
 ];
---
+++ src/app/article-dashboard/article-dashboard.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-article-dashboard",
+  standalone: true,
+  imports: [],
+  templateUrl: "./article-dashboard.component.html",
+  styleUrl: "./article-dashboard.component.scss",
+})
+export class ArticleDashboardComponent {}
