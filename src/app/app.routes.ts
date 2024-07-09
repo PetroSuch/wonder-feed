2fecc59984e6cece0bfe21e3d1806a6aeafeb8f4
Petro Sych <petro.sych@careerteacherhub.com>
2024-07-09 21:51:00 +0300

    Add the table of articles

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,17 +1,13 @@
 import { Routes } from "@angular/router";
 import { LoginComponent } from "./login/login.component";
 import { DashboardComponent } from "./dashboard/dashboard.component";
-import { ArticleDashboardComponent } from "./article-dashboard/article-dashboard.component";
 import { CreateArticleComponent } from "./create-article/create-article.component";
+import { ArticleDashboardComponent } from "./article-dashboard/article-dashboard.component";
 
 export const routes: Routes = [
   {
     path: "login",
     component: LoginComponent,
-  },
-  {
-    path: "dashboard",
-    component: DashboardComponent,
   },
   {
     path: "articles",
@@ -20,6 +16,10 @@ export const routes: Routes = [
       {
         path: "create",
         component: CreateArticleComponent,
+      },
+      {
+        path: "",
+        component: DashboardComponent,
       },
     ],
   },
--- src/app/create-article/create-article.component.ts
+++ src/app/create-article/create-article.component.ts
@@ -9,8 +9,8 @@ import {
   CdkDropList,
   CdkDropListGroup,
   moveItemInArray,
-  transferArrayItem,
 } from "@angular/cdk/drag-drop";
+import { HttpClient } from "@angular/common/http";
 
 @Component({
   selector: "app-create-article",
@@ -27,6 +27,14 @@ import {
 })
 export class CreateArticleComponent {
   public items = ["Product Title 1", "Product Title 2", "Product Title 3"];
+
+  constructor(private http: HttpClient) {
+    this.http
+      .get("http://2.58.65.29:3000/getArticlesLocal?offset=0")
+      .subscribe((data) => {
+        console.log(data);
+      });
+  }
 
   drop(event: CdkDragDrop<number[]>) {
     moveItemInArray(this.items, event.previousIndex, event.currentIndex);
